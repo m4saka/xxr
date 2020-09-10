9e5c77aca2be7ecfe88cbfff0c42a5440cec0c6e
masaka <masaka@mz-kb.com>
2020-09-11 04:07:22 +0900

    Refactor ExperimentHelper

--- include/xxr/helper/experiment_helper.hpp
+++ include/xxr/helper/experiment_helper.hpp
@@ -31,7 +31,7 @@ namespace xxr
     template <class Experiment, class Environment>
     class ExperimentHelper : public AbstractExperimentHelper
     {
-    private:
+    protected:
         const ExperimentSettings m_settings;
         std::vector<std::unique_ptr<Experiment>> m_experiments;
         std::vector<std::unique_ptr<Environment>> m_explorationEnvironments;
@@ -71,6 +71,130 @@ namespace xxr
                 );
             }
             return experiments;
+        }
+
+        virtual typename Experiment::ActionType callExperimentExploit(std::size_t idx)
+        {
+            return m_experiments[idx]->exploit(m_exploitationEnvironments[idx]->situation(), m_settings.updateInExploitation);
+        }
+
+        virtual typename Experiment::ActionType callExperimentExplore(std::size_t idx)
+        {
+            return m_experiments[idx]->explore(m_explorationEnvironments[idx]->situation());
+        }
+
+        virtual void runExploitationIteration()
+        {
+            if (m_settings.exploitationCount > 0)
+            {
+                std::size_t totalStepCount = 0;
+                double rewardSum = 0.0;
+                double systemErrorSum = 0.0;
+                double populationSizeSum = 0.0;
+                for (std::size_t j = 0; j < m_settings.seedCount; ++j)
+                {
+                    for (std::size_t k = 0; k < m_settings.exploitationCount; ++k)
+                    {
+                        do
+                        {
+                            // Choose action
+                            auto action = callExperimentExploit(j);
+
+                            // Get reward
+                            double reward = m_exploitationEnvironments[j]->executeAction(action);
+                            m_summaryRewardSum += reward / m_settings.exploitationCount / m_settings.seedCount;
+                            m_summarySystemErrorSum += std::abs(reward - m_experiments[j]->prediction()) / m_settings.exploitationCount / m_settings.seedCount;
+                            m_summaryCoveringOccurrenceRateSum += static_cast<double>(m_experiments[j]->isCoveringPerformed()) / m_settings.exploitationCount / m_settings.seedCount;
+                            if (m_settings.updateInExploitation)
+                            {
+                                m_experiments[j]->reward(reward, m_exploitationEnvironments[j]->isEndOfProblem());
+                            }
+                            rewardSum += reward;
+                            systemErrorSum += std::abs(reward - m_experiments[j]->prediction());
+                            ++totalStepCount;
+
+                            // Run callback if needed
+                            m_exploitationCallback(*m_exploitationEnvironments[j]);
+                        } while (!m_exploitationEnvironments[j]->isEndOfProblem());
+
+                        populationSizeSum += m_experiments[j]->populationSize();
+                    }
+                    m_summaryPopulationSizeSum += static_cast<double>(m_experiments[j]->populationSize()) / m_settings.seedCount;
+                }
+
+                m_summaryStepCountSum += static_cast<double>(totalStepCount) / m_settings.exploitationCount / m_settings.seedCount;
+
+                if (m_settings.summaryInterval > 0 && (m_iterationCount + 1) % m_settings.summaryInterval == 0)
+                {
+                    if (!m_alreadyOutputSummaryHeader)
+                    {
+                        if (m_settings.outputSummaryToStdout)
+                        {
+                            std::cout
+                                << "  Iteration      Reward      SysErr     PopSize  CovOccRate   TotalStep\n"
+                                << " ========== =========== =========== =========== =========== ===========" << std::endl;
+                        }
+                        if (m_summaryLogStream)
+                        {
+                            m_summaryLogStream << "Iteration,Reward,SysErr,PopSize,CovOccRate,TotalStep" << std::endl;
+                        }
+                        m_alreadyOutputSummaryHeader = true;
+                    }
+                    if (m_settings.outputSummaryToStdout)
+                    {
+                        std::printf("%11u %11.3f %11.3f %11.3f  %1.8f %11.3f\n",
+                            static_cast<unsigned int>(m_iterationCount + 1),
+                            m_summaryRewardSum / m_settings.summaryInterval,
+                            m_summarySystemErrorSum / m_settings.summaryInterval,
+                            m_summaryPopulationSizeSum / m_settings.summaryInterval,
+                            m_summaryCoveringOccurrenceRateSum / m_settings.summaryInterval,
+                            m_summaryStepCountSum / m_settings.summaryInterval);
+                        std::fflush(stdout);
+                    }
+                    if (m_summaryLogStream)
+                    {
+                        m_summaryLogStream
+                            << (m_iterationCount + 1) << ','
+                            << m_summaryRewardSum / m_settings.summaryInterval << ','
+                            << m_summarySystemErrorSum / m_settings.summaryInterval << ','
+                            << m_summaryPopulationSizeSum / m_settings.summaryInterval << ','
+                            << m_summaryCoveringOccurrenceRateSum / m_settings.summaryInterval << ','
+                            << m_summaryStepCountSum / m_settings.summaryInterval << std::endl;
+                    }
+                    m_summaryRewardSum = 0.0;
+                    m_summarySystemErrorSum = 0.0;
+                    m_summaryPopulationSizeSum = 0.0;
+                    m_summaryCoveringOccurrenceRateSum = 0.0;
+                    m_summaryStepCountSum = 0.0;
+                }
+
+                m_rewardLogStream.writeLine(rewardSum / m_settings.exploitationCount / m_settings.seedCount);
+                m_systemErrorLogStream.writeLine(systemErrorSum / m_settings.exploitationCount / m_settings.seedCount);
+                m_populationSizeLogStream.writeLine(populationSizeSum / m_settings.exploitationCount / m_settings.seedCount);
+                m_stepCountLogStream.writeLine(static_cast<double>(totalStepCount) / m_settings.exploitationCount / m_settings.seedCount);
+            }
+        }
+
+        virtual void runExplorationIteration()
+        {
+            for (std::size_t j = 0; j < m_settings.seedCount; ++j)
+            {
+                for (std::size_t k = 0; k < m_settings.explorationCount; ++k)
+                {
+                    do
+                    {
+                        // Get situation from environment and choose action
+                        auto action = callExperimentExplore(j);
+
+                        // Get reward
+                        double reward = m_explorationEnvironments[j]->executeAction(action);
+                        m_experiments[j]->reward(reward, m_explorationEnvironments[j]->isEndOfProblem());
+
+                        // Run callback if needed
+                        m_explorationCallback(*m_explorationEnvironments[j]);
+                    } while (!m_explorationEnvironments[j]->isEndOfProblem());
+                }
+            }
         }
 
     public:
@@ -123,123 +247,12 @@ namespace xxr
         {
             for (std::size_t i = 0; i < repeat; ++i)
             {
-                // Exploitation
-                if (m_settings.exploitationCount > 0)
-                {
-                    std::size_t totalStepCount = 0;
-                    double rewardSum = 0.0;
-                    double systemErrorSum = 0.0;
-                    double populationSizeSum = 0.0;
-                    for (std::size_t j = 0; j < m_settings.seedCount; ++j)
-                    {
-                        for (std::size_t k = 0; k < m_settings.exploitationCount; ++k)
-                        {
-                            do
-                            {
-                                // Choose action
-                                auto action = m_experiments[j]->exploit(m_exploitationEnvironments[j]->situation(), m_settings.updateInExploitation);
-
-                                // Get reward
-                                double reward = m_exploitationEnvironments[j]->executeAction(action);
-                                m_summaryRewardSum += reward / m_settings.exploitationCount / m_settings.seedCount;
-                                m_summarySystemErrorSum += std::abs(reward - m_experiments[j]->prediction()) / m_settings.exploitationCount / m_settings.seedCount;
-                                m_summaryCoveringOccurrenceRateSum += static_cast<double>(m_experiments[j]->isCoveringPerformed()) / m_settings.exploitationCount / m_settings.seedCount;
-                                if (m_settings.updateInExploitation)
-                                {
-                                    m_experiments[j]->reward(reward, m_exploitationEnvironments[j]->isEndOfProblem());
-                                }
-                                rewardSum += reward;
-                                systemErrorSum += std::abs(reward - m_experiments[j]->prediction());
-                                ++totalStepCount;
-
-                                // Run callback if needed
-                                m_exploitationCallback(*m_exploitationEnvironments[j]);
-                            } while (!m_exploitationEnvironments[j]->isEndOfProblem());
-
-                            populationSizeSum += m_experiments[j]->populationSize();
-                        }
-                        m_summaryPopulationSizeSum += static_cast<double>(m_experiments[j]->populationSize()) / m_settings.seedCount;
-                    }
-
-                    m_summaryStepCountSum += static_cast<double>(totalStepCount) / m_settings.exploitationCount / m_settings.seedCount;
-
-                    if (m_settings.summaryInterval > 0 && (m_iterationCount + 1) % m_settings.summaryInterval == 0)
-                    {
-                        if (!m_alreadyOutputSummaryHeader)
-                        {
-                            if (m_settings.outputSummaryToStdout)
-                            {
-                                std::cout
-                                    << "  Iteration      Reward      SysErr     PopSize  CovOccRate   TotalStep\n"
-                                    << " ========== =========== =========== =========== =========== ===========" << std::endl;
-                            }
-                            if (m_summaryLogStream)
-                            {
-                                m_summaryLogStream << "Iteration,Reward,SysErr,PopSize,CovOccRate,TotalStep" << std::endl;
-                            }
-                            m_alreadyOutputSummaryHeader = true;
-                        }
-                        if (m_settings.outputSummaryToStdout)
-                        {
-                            std::printf("%11u %11.3f %11.3f %11.3f  %1.8f %11.3f\n",
-                                static_cast<unsigned int>(m_iterationCount + 1),
-                                m_summaryRewardSum / m_settings.summaryInterval,
-                                m_summarySystemErrorSum / m_settings.summaryInterval,
-                                m_summaryPopulationSizeSum / m_settings.summaryInterval,
-                                m_summaryCoveringOccurrenceRateSum / m_settings.summaryInterval,
-                                m_summaryStepCountSum / m_settings.summaryInterval);
-                            std::fflush(stdout);
-                        }
-                        if (m_summaryLogStream)
-                        {
-                            m_summaryLogStream
-                                << (m_iterationCount + 1) << ','
-                                << m_summaryRewardSum / m_settings.summaryInterval << ','
-                                << m_summarySystemErrorSum / m_settings.summaryInterval << ','
-                                << m_summaryPopulationSizeSum / m_settings.summaryInterval << ','
-                                << m_summaryCoveringOccurrenceRateSum / m_settings.summaryInterval << ','
-                                << m_summaryStepCountSum / m_settings.summaryInterval << std::endl;
-                        }
-                        m_summaryRewardSum = 0.0;
-                        m_summarySystemErrorSum = 0.0;
-                        m_summaryPopulationSizeSum = 0.0;
-                        m_summaryCoveringOccurrenceRateSum = 0.0;
-                        m_summaryStepCountSum = 0.0;
-                    }
-
-                    m_rewardLogStream.writeLine(rewardSum / m_settings.exploitationCount / m_settings.seedCount);
-                    m_systemErrorLogStream.writeLine(systemErrorSum / m_settings.exploitationCount / m_settings.seedCount);
-                    m_populationSizeLogStream.writeLine(populationSizeSum / m_settings.exploitationCount / m_settings.seedCount);
-                    m_stepCountLogStream.writeLine(static_cast<double>(totalStepCount) / m_settings.exploitationCount / m_settings.seedCount);
-                }
-
-                // Exploration
-                for (std::size_t j = 0; j < m_settings.seedCount; ++j)
-                {
-                    for (std::size_t k = 0; k < m_settings.explorationCount; ++k)
-                    {
-                        do
-                        {
-                            // Get situation from environment
-                            auto situation = m_explorationEnvironments[j]->situation();
-
-                            // Choose action
-                            auto action = m_experiments[j]->explore(situation);
-
-                            // Get reward
-                            double reward = m_explorationEnvironments[j]->executeAction(action);
-                            m_experiments[j]->reward(reward, m_explorationEnvironments[j]->isEndOfProblem());
-
-                            // Run callback if needed
-                            m_explorationCallback(*m_explorationEnvironments[j]);
-                        } while (!m_explorationEnvironments[j]->isEndOfProblem());
-                    }
-                }
-
+                runExploitationIteration();
+                runExplorationIteration();
                 ++m_iterationCount;
             }
         }
-        
+
         virtual void switchToCondensationMode() override
         {
             for (auto && experiment : m_experiments)
